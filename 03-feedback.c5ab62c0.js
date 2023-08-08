600ecf40067b7a67321a3887822f867cc6ae7e90
Serhii120796 <128705114+Serhii120796@users.noreply.github.com>
2023-08-08 18:18:25 +0000

    Deploying to gh-pages from @ Serhii120796/goit-js-hw-08@24ad0bd5d52b67bde4c02e4ef9506614a1d74cdb 🚀

---
+++ 03-feedback.c5ab62c0.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){console.log(e.target.value)}));
+//# sourceMappingURL=03-feedback.c5ab62c0.js.map
---
+++ 03-feedback.c8c8f0d8.js
@@ -0,0 +1,2 @@
+document.querySelector(".feedback-form").addEventListener("input",(function(e){console.log(e.target.value)}));
+//# sourceMappingURL=03-feedback.c8c8f0d8.js.map
