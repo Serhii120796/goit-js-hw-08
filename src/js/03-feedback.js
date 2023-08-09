b43897eaa8c936fb6ffd849f29939cdcae1d5569
Serhii120796 <sfedyura@ukr.net>
2023-08-09 10:41:58 +0300

    Update 03-feedback.js

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,6 +1,24 @@
-const form = document.querySelector('.feedback-form');
-form.addEventListener('input', onInput);
+import throttle from 'lodash.throttle';
 
-function onInput(evt) {
-    console.log(evt.target.value);
-}
+const form = document.querySelector('.feedback-form');
+
+const { email, message } = form.elements;
+
+form.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem('email', email.value);
+    localStorage.setItem('message', message.value);
+  }, 500)
+);
+
+email.value = '' ? '' : localStorage.getItem('email');
+message.value = '' ? '' : localStorage.getItem('message');
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  console.log({ email: email.value, message: message.value });
+  email.value = "";
+  message.value = "";
+  localStorage.clear();
+});
